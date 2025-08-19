c62556da09a51c85da082c9e1448e51a274aa6d6
BenLarry <4muki@gmail.com>
2025-08-19 19:32:50 +0300

    moduuli3 tehtävä 4 ratkaisu

---
+++ moduuli_03/teht04.py
@@ -0,0 +1,7 @@
+year = int(input("Anna minulle vuosiluku: "))
+
+if year % 4 == 0:
+    if year % 100 != 0 or year % 400 == 0:
+        print(f"{year} on karkausvuosi")
+else:
+    print(f"{year} ei ole karkausvuosi")
