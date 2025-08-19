97c3b791526291d856676419fe4888691265d2a4
BenLarry <4muki@gmail.com>
2025-08-19 13:44:23 +0300

    moduuli2 tehtävä 4 ratkaisu

---
+++ moduuli_02/teht04.py
@@ -0,0 +1,9 @@
+num_one = int(input("anna ensimmäinen numero: "))
+num_two = int(input("anna toinen numero: "))
+num_three = int(input("anna kolmas numero: "))
+
+summa = num_one + num_two + num_three
+tulo = num_one * num_two * num_three
+keskiarvo = (num_one + num_two + num_three) / 3
+
+print(f"lukujen summa: {summa}, lukujen tulo: {tulo} ja lukujen keskiarvo {keskiarvo}")
