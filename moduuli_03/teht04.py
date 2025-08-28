44c63aaf09a4552364ecdbdec4d89d3a555a3d7b
BenLarry <4muki@gmail.com>
2025-08-28 10:07:22 +0300

    karkaus vuosi korjaus

--- moduuli_03/teht04.py
+++ moduuli_03/teht04.py
@@ -1,7 +1,6 @@
 year = int(input("Anna minulle vuosiluku: "))
 
-if year % 4 == 0:
-    if year % 100 != 0 or year % 400 == 0:
-        print(f"{year} on karkausvuosi")
+if year % 4 == 0 and year % 100 != 0 or year % 400 == 0:
+    print(f"{year} on karkausvuosi")
 else:
     print(f"{year} ei ole karkausvuosi")
