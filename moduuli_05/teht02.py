d87535fa964746e13b8d4228496a0dc5be6bc9c6
BenLarry <4muki@gmail.com>
2025-09-02 10:34:28 +0300

    rewrite

--- moduuli_05/teht02.py
+++ moduuli_05/teht02.py
@@ -3,16 +3,10 @@ number = input("Kirjoita luku, tyhjä merkki lopettaa ohjelman: ")
 
 list_of_numbers = []
 
-while number != " ":
+while number != "":
     list_of_numbers.append(int(number))
     number = input("Kirjoita luku: ")
 
 list_of_numbers.sort(reverse=True)
 
-largest_nums = []
-for i, num in enumerate(list_of_numbers):
-    if i == 5:
-        break
-    largest_nums.append(num)
-
-print(f"suurimmat luvut ovat: {largest_nums}")
+print(f"suurimmat luvut ovat: {list_of_numbers[0:5]}")
