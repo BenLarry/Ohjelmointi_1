37fc9f9fdbda37747049e0c997d67f79c22f1e02
BenLarry <4muki@gmail.com>
2025-09-11 20:48:22 +0300

    bugin korjaus

--- moduuli_07/teht02.py
+++ moduuli_07/teht02.py
@@ -1,15 +1,17 @@
 names = set()
 
-name = None
+name = input("Kirjoita nimi(tyhjä merkkijono lopettaa objelman): ")
 while name != "":
-    name = input("Kirjoita nimi(tyhjä merkkijono lopettaa objelman): ")
     if name not in names:
         print("Uusi nimi syötetty")
         names.add(name)
     else:
         print("Nimi syötetty aijemmin")
     
+    name = input("Kirjoita nimi(tyhjä merkkijono lopettaa objelman): ")
+
+print(f"Kaikki nimet syötetyt nimet: {names}")
 
 
 
-
+##BUGINEN
