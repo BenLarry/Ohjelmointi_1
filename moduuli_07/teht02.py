3319227e69bf4dd3a1fe8acf99a3833eb75d2760
BenLarry <4muki@gmail.com>
2025-09-11 09:46:43 +0300

    tehtävän yksinkertaminen, ohjeet ymmäretty väärin

--- moduuli_07/teht02.py
+++ moduuli_07/teht02.py
@@ -4,11 +4,10 @@ name = None
 while name != "":
     name = input("Kirjoita nimi(tyhjä merkkijono lopettaa objelman): ")
     if name not in names:
-        print("new: ", name)
+        print("Uusi nimi syötetty")
         names.add(name)
-        last_name = name
     else:
-        print("last: ", last_name)
+        print("Nimi syötetty aijemmin")
     
 
 
