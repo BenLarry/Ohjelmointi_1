24f1d82dc66633a43c7bbcfc52546b748c0f5a75
BenLarry <4muki@gmail.com>
2025-10-21 14:05:40 +0300

    lisäsin km tekstiin ja m

--- moduuli_09/teht01.py
+++ moduuli_09/teht01.py
@@ -7,7 +7,7 @@ class Auto():
 
 def main():
     car = Auto("ABC-123", 142)
-    print(f"rekisterutunnus: {car.registration_number}, Huippunopeus: {car.top_speed}, Nopeus: {car.current_speed}, ajettumatka: {car.travel_distance}")
+    print(f"rekisterutunnus: {car.registration_number}, Huippunopeus: {car.top_speed} km/h, Nopeus: {car.current_speed} km/h, ajettumatka: {car.travel_distance} km")
 
 
 main()
