e4ba4cbb1eb5bcadbd0686796f7716da8f3554e3
BenLarry <4muki@gmail.com>
2025-10-21 14:12:57 +0300

    tehtävä 2 ratkaisu

---
+++ moduuli_09/teht02.py
@@ -0,0 +1,22 @@
+class Auto():
+    def __init__(self, registration_number, top_speed):
+        self.registration_number = registration_number
+        self.top_speed = top_speed
+        self.current_speed = 0
+        self.travel_distance = 0
+
+    def accelerate(self, speed):
+        self.current_speed += speed
+
+def main():
+    car = Auto("ABC-123", 142)
+    print(f"rekisterutunnus: {car.registration_number}, Huippunopeus: {car.top_speed} km/h, Nopeus: {car.current_speed} km/h, ajettumatka: {car.travel_distance} km")
+
+    
+    car.accelerate(30)
+    car.accelerate(70)
+    car.accelerate(50)
+    car.accelerate(-200)
+    print(car.current_speed)
+
+main()
