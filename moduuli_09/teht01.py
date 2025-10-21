74fc757dfda99e586fff7487a773b65d52f0e0f0
BenLarry <4muki@gmail.com>
2025-10-21 14:03:36 +0300

    tehtävä 1 ratkaisu

---
+++ moduuli_09/teht01.py
@@ -0,0 +1,13 @@
+class Auto():
+    def __init__(self, registration_number, top_speed):
+        self.registration_number = registration_number
+        self.top_speed = top_speed
+        self.current_speed = 0
+        self.travel_distance = 0
+
+def main():
+    car = Auto("ABC-123", 142)
+    print(f"rekisterutunnus: {car.registration_number}, Huippunopeus: {car.top_speed}, Nopeus: {car.current_speed}, ajettumatka: {car.travel_distance}")
+
+
+main()
